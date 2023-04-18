3eb55330431a7ef636c1f1e7497c3c0d2929370d
Samantha Navarro <samantha.m.navarro@gmail.com>
2023-04-17 17:43:57 -0700

    filter function still needs fixing

--- infinite-scroll-fetch-api/script.js
+++ infinite-scroll-fetch-api/script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // fetching post using async/await
@@ -40,5 +40,58 @@ async function showPosts() {
   });
 }
 
+// Show loader and show more posts
+// add class of show
+// setTimeout will only allow the loader to show for an x amount
+// 2nd timeout will allow the posts to show with the time limit set
+
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// filter posts
+
+function filterPosts(e) {
+  // check to see if the input is working
+
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  // will loop throught the posts of the title and body to match
+
+  posts.forEach((post) => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // will show intial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  // from stackoverflow
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    // console.log(123)
+    // will show loading circles
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
